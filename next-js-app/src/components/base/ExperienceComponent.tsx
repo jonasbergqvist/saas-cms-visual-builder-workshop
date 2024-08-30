163df2d57f92b368b5212def3bf238f96fa89ffd
Jonas Bergqvist <jonas.bergqvist@episerver.com>
2024-08-30 13:35:48 +0200

    handle styles

--- next-js-app/src/components/base/ExperienceComponent.tsx
+++ next-js-app/src/components/base/ExperienceComponent.tsx
@@ -2,6 +2,7 @@ import { graphql } from "@/graphql/gql";
 import { optiGraphClient } from "@/optiGraphClient";
 import { FC } from "react";
 import SimpleElementComponent from "../elements/SimpleElementComponent";
+import { CompositionDisplaySetting } from "@/graphql/graphql";
 
 export const ExperienceQuery = graphql(/* GraphQL */ `
     query Experience($url: String, $version: String) {
@@ -73,10 +74,36 @@ interface props {
 }
  
 const ExperienceComponent: FC<props> = async ({ url, version }) => {
-  const data = await optiGraphClient.request(ExperienceQuery, {
+
+    function getGridClassName(displaySettings: CompositionDisplaySetting[]): string {
+        let className = "relative w-full flex flex-col flex-nowrap justify-start vb:grid ";
+
+        displaySettings.forEach((displaySetting) => {
+            if(displaySetting.key === "colorScheme") {
+                switch(displaySetting.value)
+                {
+                    case "primary":
+                        className += "bg-sky-200 "
+                        break;
+                    case "secondary":
+                        className += "bg-sky-800 "
+                        break;
+                    default:
+                        className += " "
+                }
+            } else if(displaySetting.key === "highlight" && displaySetting.value === "true") {
+                className += "text-4xl "
+            }
+        });
+    
+        return className
+    }
+
+    const data = await optiGraphClient.request(ExperienceQuery, {
     url,
     version,
-  });
+    });
+
     return (
         <div key="unstructuredData" className="relative w-full flex-1 vb:outline">
             {
@@ -87,7 +114,7 @@ const ExperienceComponent: FC<props> = async ({ url, version }) => {
                       experience?.composition?.nodes?.map((grid) => {
                           if(grid?.__typename === "CompositionStructureNode") {
                               return (
-                                  <div className="relative w-full flex flex-col flex-nowrap justify-start vb:grid" data-epi-block-id={grid?.key} key={grid.key}>
+                                  <div className={getGridClassName(grid?.displaySettings as CompositionDisplaySetting[])} data-epi-block-id={grid?.key} key={grid.key}>
                                       {
                                         grid.nodes?.map((row) => {
                                           if(row?.__typename === "CompositionStructureNode") {
